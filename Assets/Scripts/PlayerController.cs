e772cfa2c8a72c6b52ec5186334dbb5180b403c9
Wallace Viguier <wallace.viguier@evadiagnostics.com>
2015-08-09 14:55:27 +0100

    Updated player Controller script to avoid double Jump and Mode when arrow keys hold down.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,27 +2,40 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
+	
+	// Variables
 	public float jumpHeight;
-	public float moveSpeed;
-
+	public float moveSpeedInc = 1;
+	public float moveSpeedMax = 3;
+	public bool isjumping;
+	
 	// Use this for initialization
 	void Start () {}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if (Input.GetKeyDown(KeyCode.UpArrow))
+		//Jump
+		if (Input.GetKey(KeyCode.UpArrow)&& (!isjumping))
 		{
-				GetComponent<Rigidbody2D>().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jumpHeight);
-	    }
-		if (Input.GetKeyDown(KeyCode.RightArrow))
-		{
-			GetComponent<Rigidbody2D>().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+			isjumping = true;	
+			GetComponent<Rigidbody2D>().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jumpHeight);
 		}
-		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		
+		//Move Forward
+		if ((Input.GetKey(KeyCode.RightArrow)) && (GetComponent<Rigidbody2D>().velocity.x < moveSpeedMax))
 		{
-			GetComponent<Rigidbody2D>().velocity = new Vector2 (-moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+			GetComponent<Rigidbody2D>().velocity += new Vector2(moveSpeedInc, 0);
 		}
+		
+		//Move Backward
+		if ((Input.GetKey(KeyCode.LeftArrow)) && (GetComponent<Rigidbody2D>().velocity.x > -moveSpeedMax))
+		{
+			GetComponent<Rigidbody2D>().velocity += new Vector2(-moveSpeedInc, 0);
+		}
+	}
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		isjumping = false;
 	}
 }
